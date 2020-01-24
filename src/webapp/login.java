19c66b150836e8142b650eb07384e6ec61b28cd5
Costin <costin.gugiu@gmail.com>
2020-01-25 00:23:00 +0200

    1.1
    ~ Renamed Login

--- src/webapp/login.java
+++ src/webapp/login.java
@@ -1,20 +1,20 @@
-package webapp;
+package webApp;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class login extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+public class Login extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        PrintWriter out = response.getWriter();
-        out.println("Oh, welcome back, <i>" + request.getParameter("username") + "</i>. Your password, <u>" + request.getParameter("password") + "</u> is nice!");
+//        PrintWriter out = response.getWriter();
+//        out.println("Oh, welcome back, <i>" + request.getParameter("username") + "</i>. Your password, <u>" + request.getParameter("password") + "</u> is nice!");
 
-    }
+        request.setAttribute("user",  request.getParameter("username")); //new variable "user" = login's input field called "username"
+        request.setAttribute("pass",  request.getParameter("password")); //new variable "pass" = login's input field called "password"
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/welcome.jsp");
     }
 }
